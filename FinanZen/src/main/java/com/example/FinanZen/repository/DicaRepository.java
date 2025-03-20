1011167d1c423ce42a51eda253807095d24e983d
Cassia Gomes <128179609+cassiaagomes@users.noreply.github.com>
2025-03-19 21:09:32 -0300

    Update DicaRepository.java

--- FinanZen/src/main/java/com/example/FinanZen/repository/DicaRepository.java
+++ FinanZen/src/main/java/com/example/FinanZen/repository/DicaRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DicaRepository extends JpaRepository<Dica, Long> {
-    // Não é necessário adicionar métodos customizados se você for usar os métodos padrões do JpaRepository
-    // Caso queira adicionar um método customizado para inserir várias dicas:
     // List<Dica> saveAll(List<Dica> dicas);
 }
